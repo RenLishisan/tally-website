bca9562b6d41d3e424f6085c8f1e4082b7e5fe54
RenLishisan <renshsian@qq.com>
2020-08-09 21:31:21 +0800

    deploy

---
+++ precache-manifest.ba3bdbaca531123b2176a112a47afffd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ce8837fae2857b3c942",
+    "url": "/tally-website/css/app.9fe2d49e.css"
+  },
+  {
+    "revision": "26b00a804c60f3b68d72",
+    "url": "/tally-website/css/chunk-vendors.6dc57fe0.css"
+  },
+  {
+    "revision": "418e58e9a62c8ea805397bb2c5045e26",
+    "url": "/tally-website/index.html"
+  },
+  {
+    "revision": "6ce8837fae2857b3c942",
+    "url": "/tally-website/js/app.167874b4.js"
+  },
+  {
+    "revision": "26b00a804c60f3b68d72",
+    "url": "/tally-website/js/chunk-vendors.e4730fe7.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/tally-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tally-website/robots.txt"
+  }
+]);
